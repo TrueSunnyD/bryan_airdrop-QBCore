c2c6c33c1c3c3c2097db0cf87e0f4c64ea8fb60b
TrueSunnyD <82911075+TrueSunnyD@users.noreply.github.com>
2022-09-26 21:24:30 -0600

    2nd commit, still needs work

--- config.lua
+++ config.lua
@@ -6,17 +6,17 @@ Config.PlayerLoaded = 'QBCore:OnPlayerLoaded' --untested
 Config.Locale = 'en'
 
 -- Default: false | This just prints things in console
-Config.Debug = false
+Config.Debug = true
 
 Config.Airdrops = {
     Command = { -- Command which spawns Airdrop | Command can be disabled: enable = false | You can configure the name of the command and also who can execute it
         enabled = true,
         name = 'airdrop',
-        groups = {'admin', 'superadmin'}
+        groups = {'admin', 'god'}
     },
     OnlyCommand = false, -- true = only admins with commands can spawn in airdrops | false = time interval between automatic spawn (command will also be active)
     CoordsOfCommand = true, -- Spawn Airdrop on command executor's location
-    Cooldown = { min = 5, max = 15 }, -- Minutes | Cooldown time before next airdrop spawns in | Default: { min = 5, max = 15 }
+    Cooldown = { min = 1, max = 2 }, -- Minutes | Cooldown time before next airdrop spawns in | Default: { min = 5, max = 15 }
     CommandRestart = false, -- Does Command (if enabled) restart Cooldown time of next airdrop | Default: false
     FallSpeed = 1, -- Try modifying this to see how fast You want the Airdrop to go down | Lowering number, means slowing down airdrop fall speed | Default: 1
     SpawnOffline = false, -- Will Airdrops spawn when there're no players online | Default: false
@@ -29,7 +29,7 @@ Config.Airdrops = {
 Config.Locations = {
     vector3(-55.02, -1764.84, 200.0),
     vector3(-93.91, -1749.73, 200.0),
-    vector3(-139.08, -1712.92, 200.0)
+    --vector3(-139.08, -1712.92, 200.0)
 }
 
 -- Loot Table is chosen randomly, unless admin spawns it with specific ID. If randomly chosen Loot Table doesn't meet chance %, the script will automatically try to select other Loot Table
@@ -62,10 +62,11 @@ Config.Blip = {
 }
 
 QBCore = exports['qb-core']:GetCoreObject()
+local src = source
 -- Functions
 Config.Notification = function(msg) -- Client Side | Custom Notification if You want | By Default: ESX Notification
     --TriggerEvent('esx:showNotification', msg)
-    QBCore.Functions.Notify(msg)
+    TriggerClientEvent('QBCore:Notify', src, msg)
     
 end
 
@@ -77,9 +78,10 @@ end
 
 Config.NotifyPlayers = function(msg, coords) -- Server Side | Should be perhaps some kind of email to the player's phone | For testing it is made as Default ESX Notification
     --TriggerClientEvent('esx:showNotification', -1, msg)
-    QBCore.Functions.Notify(msg)
+    TriggerClientEvent('QBCore:Notify', src, msg)
 end
 
 Config.NotifyExecute = function(playerId, xPlayer) -- Server Side | This function is triggered when someone spawn in airdrop through command | You can add logs for example here and get identifier, name from playerId or xPlayer
 
 end
+
